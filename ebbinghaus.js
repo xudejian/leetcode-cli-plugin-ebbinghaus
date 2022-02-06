9d886d926cb84fec575327643a4975529181de13
xudejian <xudejian2008@gmail.com>
2022-02-06 01:05:59 -0800

    fix: keep days in ac as int, because Object.keys will change everything to string

--- ebbinghaus.js
+++ ebbinghaus.js
@@ -1,11 +1,14 @@
 var moment = require('moment');
 var _ = require('underscore');
 
+var chalk = require('../chalk');
+var icon = require('../icon');
 var log = require('../log');
 var Plugin = require('../plugin');
 var session = require('../session');
+var sprintf = require('../sprintf');
 var h = require('../helper');
-const list_aliases = require('../commands/list').aliases;
+const stat_aliases = require('../commands/stat').aliases;
 const show_aliases = require('../commands/show').aliases;
 
 var plugin = new Plugin(300, 'ebbinghaus', '2021.12.30',
@@ -17,26 +20,27 @@ function getEbbinghaus() {
 	for (let k of _.keys(stats)) {
 		const d = moment().diff(moment(k, 'YYYY-MM-DD'), 'days');
 		(stats[k]['ac.set']||[]).forEach(function(id) {
-			ac[id] = ac[id] || {};
-			ac[id][d] = true;
+			ac[id] = ac[id] || new Set();
+			ac[id].add(d);
 		});
 	}
 
 	const ebbinghaus = {};
 	for (let id of _.keys(ac)) {
-		if (ac[id][0]||false) {
+		if (ac[id].has(0)) {
 			continue;
 		}
-		const days = _.keys(ac[id]).sort();
+		const days = Array.from(ac[id]).sort(function(a, b) { return a - b; });
 		const task_days            = [1, 2, 4, 7, 15, 30, 90, 180];
 		const next_task_after_days = [1, 1, 2, 3,  8, 15, 60, 90];
 		let i = days.length-1;
 		let j = i-1;
 		let k = 0;
+		log.debug('['+id+'] review: ' + JSON.stringify(days));
 		while (i > 0 && j>=0) {
 			const d = days[i] - days[j];
 			if (d < next_task_after_days[k]) {
-				log.debug('review early than expect: ' + id);
+				log.debug('review early: ' + id);
 				j--;
 			} else if (d == next_task_after_days[k]) {
 				log.debug('review as expect: ' + id + ' in days ' + task_days[k]);
@@ -48,7 +52,7 @@ function getEbbinghaus() {
 				}
 				j = i-1;
 			} else {
-				log.debug('review later than expect: ' + id + ' in days ' + task_days[k]);
+				log.debug('review later: ' + id + ' in days ' + task_days[k]);
 				i--;
 			}
 		}
@@ -56,7 +60,7 @@ function getEbbinghaus() {
 			log.debug('complete full cycle: ' + id);
 			continue;
 		}
-		log.debug('previous day:' + i + days[i] + ' expect wait ' + k + ' cur ' + j);
+		log.debug('previous day:' + i + ' ' + days[i] + ' expect wait ' + k + ' cur ' + j);
 		if (days[i] >= next_task_after_days[k]) {
 			ebbinghaus[id] = task_days[k];
 		}
@@ -64,22 +68,45 @@ function getEbbinghaus() {
 	return ebbinghaus;
 }
 
-function getProblems(needTranslation, cb) {
+function statProblems(needTranslation, cb) {
   plugin.next.getProblems(needTranslation, function(e, problems) {
     if (e) return cb(e);
 
-		const ebbinghaus = getEbbinghaus();
-		problems = problems.filter(function(x) {
-			if ((ebbinghaus[x.fid]||0) > 0) {
-				x.tags = (x.tags || []).concat(['days-' + ebbinghaus[x.fid]]);
-				x.state = 'review';
-				return true;
-			}
-			return false;
-		});
-		log.debug('list in ebbinghaus');
-    return cb(null, problems);
+		const ac = {};
+		const stats = require('../cache').get(h.KEYS.stat) || {};
+		for (let k of _.keys(stats)) {
+			const d = moment().diff(moment(k, 'YYYY-MM-DD'), 'days');
+			(stats[k]['ac.set']||[]).forEach(function(id) {
+				ac[id] = ac[id] || {};
+				ac[id][d] = true;
+			});
+		}
+		showGraph(ac);
   });
+}
+
+function showGraph(problems) {
+  const ICONS = {
+    ac:    chalk.green(icon.ac),
+    notac: chalk.red(icon.notac),
+    none:  chalk.gray(icon.none),
+    empty: icon.empty
+  };
+
+	for (const [key, value] of Object.entries(problems)) {
+		let line = new Array(95).fill(ICONS.none);
+		let fid = sprintf(' %04s', key);
+		for (let i=0; i<fid.length; i++) {
+			line[i] = fid[i];
+		}
+		for (let d of Object.keys(value)) {
+			if (d <= 90) {
+				line[94-d] = ICONS.ac;
+			}
+		}
+    log.info(line.join(''));
+	}
+  log.info();
 }
 
 function pickProblem(needTranslation, cb) {
@@ -111,9 +138,9 @@ function pickProblem(needTranslation, cb) {
   });
 }
 
-const aliases = {'list':getProblems, 'show':pickProblem};
-for (let a of list_aliases) {
-	aliases[a] = getProblems;
+const aliases = {'stat':statProblems, 'show':pickProblem};
+for (let a of stat_aliases) {
+	aliases[a] = statProblems;
 }
 for (let a of show_aliases) {
 	aliases[a] = pickProblem;
